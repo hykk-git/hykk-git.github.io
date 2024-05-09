cf4044c28fdfab30243fb2c2e0cc777fdf7dfc24
hykk-git <gratia1206@naver.com>
2024-05-09 10:19:50 +0900

    upload: week 10

---
+++ express_lab/stocks-api.js
@@ -0,0 +1,19 @@
+const path = require('path'); 
+const express = require('express');
+const app = express();
+const provider = require('./scripts/data-provider.js');
+const stocks = provider.data;
+app.use('/static', 
+ express.static(path.join(__dirname,'public')));
+
+// set up route handling
+const router = require('./scripts/stock-router.js'); 
+router.handleAllStocks(app); 
+router.handleSingleSymbol(app); 
+router.handleNameSearch(app);
+
+// Use express to listen to port
+let port = process.env.PORT; 
+app.listen(port, () => { 
+ console.log("Server running at port= " + port); 
+});
